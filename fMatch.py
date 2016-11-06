c76e3f68ed5aac50a8b01bc4085010fdbdf45ed2
shashankYadav <shashank7.iitd@gmail.com>
2016-11-06 13:14:57 +0530

    Code Completed

--- fMatch.py
+++ fMatch.py
@@ -2,7 +2,10 @@ import cv2
 import math
 import numpy as np
 
-def Cm(fi,fj):
+def CmList(fi,fjList):
+
+	# fi = cv2.cvtColor(fi, cv2.COLOR_BGR2GRAY)
+	# fj = cv2.cvtColor(fj, cv2.COLOR_BGR2GRAY)
 
 	d = np.sqrt( fi.shape[0]*fi.shape[0] + fi.shape[1]*fi.shape[1] )
 	tc = 0.1*d
@@ -22,49 +25,57 @@ def Cm(fi,fj):
 
 	p0 = cv2.goodFeaturesToTrack(fi, mask = None, **feature_params)
 
-	# calculate optical flow
-	p1, st, err = cv2.calcOpticalFlowPyrLK(fi, fj, p0, None, **lk_params)
-	good_new = p1[st==1]
-	good_old = p0[st==1]
+	motionCostArr = np.zeros(fjList.size)
+	it = 0
 
-	h, status = cv2.findHomography(good_old, good_new)
-	
-	Cm = 0.0
+	for fj in fjList:
+		# calculate optical flow
+		p1, st, err = cv2.calcOpticalFlowPyrLK(fi, fj, p0, None, **lk_params)
+		good_new = p1[st==1]
+		good_old = p0[st==1]
 
-	pt1 = np.ones([3,1])
-	pt2 = np.ones([3,1])
-	pt1 = np.asmatrix(pt1)
-	pt2 = np.asmatrix(pt2)
-	
-	for x in xrange(0,good_old.shape[0]):
-		pt1[0,0] = good_old[x,0]
-		pt1[1,0] = good_old[x,1]
+		h, status = cv2.findHomography(good_old, good_new)
+		
+		Cm = 0.0
+
+		pt1 = np.ones([3,1])
+		pt2 = np.ones([3,1])
+		pt1 = np.asmatrix(pt1)
+		pt2 = np.asmatrix(pt2)
+		
+		for x in xrange(0,good_old.shape[0]):
+			pt1[0,0] = good_old[x,0]
+			pt1[1,0] = good_old[x,1]
+			pt1[2,0] = 1
+
+			pt1 = np.mat(h)*pt1
+
+			pt2[0,0] = good_new[x,0]
+			pt2[1,0] = good_new[x,1]
+			pt2[2,0] = 1
+
+			Cm += np.linalg.norm(pt2-pt1)
+
+		Cm /= good_old.shape[0]
+		
+		pt1[0,0] = good_old.shape[0]/2
+		pt1[1,0] = good_old.shape[1]/2
 		pt1[2,0] = 1
+		
+		pt2 = np.mat(h)*pt1
 
-		pt1 = np.mat(h)*pt1
+		C0 = np.linalg.norm(pt2-pt1)
+		
+		# print Cm , C0 , tc , gamma
 
-		pt2[0,0] = good_new[x,0]
-		pt2[1,0] = good_new[x,1]
-		pt2[2,0] = 1
+		if Cm < tc:
+			motionCostArr[it] = C0
+		else:
+			motionCostArr[it] = gamma
+		it += 1
 
-		Cm += np.linalg.norm(pt2-pt1)
+	return motionCostArr
 
-	Cm /= good_old.shape[0]
-	
-	pt1[0,0] = good_old.shape[0]/2
-	pt1[1,0] = good_old.shape[1]/2
-	pt1[2,0] = 1
-	
-	pt2 = np.mat(h)*pt1
-
-	C0 = np.linalg.norm(pt2-pt1)
-	
-	# print Cm , C0 , tc , gamma
-
-	if Cm < tc:
-		return C0
-	else:
-		return gamma
 
 
 def Cs(i,j,v):
@@ -75,4 +86,19 @@ def Cs(i,j,v):
 
 def Ca(h,i,j):
 	ta = 200
-	return min(math.fabs((j-i) - (i-h) ),ts)
+	return min(math.fabs((j-i) - (i-h) ),ta)
+
+
+def getMin(D,i,j,lamdaA):
+	argmin = 0
+	minVal = float("inf")
+	l = []
+	for k in xrange( 1 , D.shape[1]+1):
+		val = D[i-k,k-1] + lamdaA*Ca(i-k,i,j)
+		l.append(val)
+		if val < minVal:
+			minVal = val
+			argmin = i-k
+	# print l
+
+	return (minVal,argmin) 
--- main.py
+++ main.py
@@ -1,9 +1,107 @@
-from fMatch import * 
+from fMatch import *
 
-f1 = cv2.imread('road1.png')
-f1 = cv2.cvtColor(f1, cv2.COLOR_BGR2GRAY)
+w = 10
+g = 10
+v = 4
+batchSize = 100
+lamdaS = 10
+lamdaA = 10
 
-f2 = cv2.imread('road2.png')
-f2 = cv2.cvtColor(f2, cv2.COLOR_BGR2GRAY)
+# cap = cv2.VideoCapture("test.mp4")
+cap = cv2.VideoCapture("roadedge0.mp4")
+ret, frame = cap.read()
+temporalNeighbors = np.zeros([batchSize,frame.shape[0],frame.shape[1]],dtype = np.uint8)
+fCount = 0
+it = 0
+MCList = []
 
-print Cm(f1,f2)
+while ret:
+	
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	
+	if it == batchSize:
+		motionCostArr = np.zeros(w)
+		for i in xrange(0,batchSize-w):
+			fi = temporalNeighbors[i,:,:]
+			fjList = temporalNeighbors[range(i+1,i+w+1),:,:]
+			MCList.append(CmList(fi,fjList))
+
+		temporalNeighbors[range(0,w),:,:] = temporalNeighbors[range(batchSize-w,batchSize),:,:]
+		it = w 		
+
+	temporalNeighbors[it,:,:] = frame
+	it += 1
+	ret, frame = cap.read()
+	fCount += 1
+
+motionCostArr = np.zeros(w)
+print len(MCList)
+for i in xrange(0,it-w):
+	fi = temporalNeighbors[i,:,:]
+	fjList = temporalNeighbors[range( i+1 , i+w+1),:,:]
+	toAppend = CmList(fi,fjList)
+	# if len(toAppend) > 0:
+	MCList.append(toAppend)
+
+
+
+# Initialization
+D = np.zeros([len(MCList),w])
+T = np.zeros([len(MCList),w])
+
+for i in xrange(0,g):
+	for j in xrange(0,w):
+		D[i,j] = MCList[i][j]+ lamdaS*Cs(i,j+i+1,v)
+
+
+# First pass: populate Dv
+for i in xrange(g,len(MCList)):
+	for j in xrange(0,w):
+		c = MCList[i][j] + lamdaS*Cs(i,j+i+1,v)
+		minVal , argmin = getMin( D, i , j+i+1 ,lamdaA )
+		D[i,j] = c + minVal
+		T[i,j] = argmin
+
+
+# Second pass: trace back min cost path
+t = len(MCList)
+
+s = -1
+d = -1
+minVal = float("inf")
+
+for i in xrange(t-g,t):
+	for j in xrange(0,w):
+		print i,j,D[i,j]
+		if D[i,j] < minVal:
+			minVal = D[i,j] 
+			s,d = i,j
+
+p = [s+d+1]
+while s > g:
+	p.append(s)
+	b = T[s,d]
+	d = s - (b+1)
+	s = b
+
+p.reverse()
+
+cap = cv2.VideoCapture("roadedge0.mp4")
+ret, frame = cap.read()
+fCount = 0
+it = 0
+fourcc = cv2.cv.CV_FOURCC(*'XVID')
+size = (frame.shape[1], frame.shape[0])
+vid = cv2.VideoWriter("hyperLapse.avi", fourcc, 10 , size, True)
+
+
+while ret and it < len(p): 
+	if fCount == p[it]:
+		vid.write(frame)
+		it += 1
+	ret,frame = cap.read()
+	print fCount
+	fCount += 1
+
+
+vid.release()
